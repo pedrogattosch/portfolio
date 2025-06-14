e6898698efd8eb5727d99aa1e942d81ce79934e7
pedrogattosch <pedrogattosch@gmail.com>
2025-06-13 22:59:49 -0300

    docs: update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,24 +2,50 @@ import React from "react";
 
 function Projects() {
   return (
-    <section id='projetos'>
-      <div className='container m-auto px-5 py-5'>
-        <h2 className='text-xl font-semibold'>Projetos</h2>
-        <div className='flex flex-col sm:flex-row gap-10 mt-10'>
-          <div className='border border-gray-500 rounded-md p-5 flex-1'>
-            <h3 className='text-2xl font-semibold mt-8'>Em construção...</h3>
-            <p className='text-gray-400 text-sm mt-4'>Ainda estou desenvolvendo, aguarde um pouco.</p>
-            <button className='mt-5 underline hover:scale-105 transition-transform'>Visualizar o código</button>
+    <section id="projetos">
+      <div className="container m-auto px-5 py-5">
+        <h2 className="text-xl font-semibold">Projetos</h2>
+        <div className="flex flex-col sm:flex-row gap-10 mt-10">
+          <div className="border border-gray-500 rounded-md p-5 flex-1">
+            <h3 className="text-2xl font-semibold mt-8">
+              Classificador de gatos e cachorros
+            </h3>
+            <p className="text-gray-400 text-sm mt-4">
+              Este projeto é uma solução de um exercício de Machine Learning da
+              freeCodeCamp. O objetivo é treinar um modelo que identifica se uma
+              imagem contém um gato ou um cachorro, utilizando redes neurais
+              convolucionais.
+            </p>
+            <a
+              href="https://github.com/pedrogattosch/cat-dog"
+              target="_blank"
+              className="mt-5 underline hover:scale-105 transition-transform"
+            >
+              Visualizar o código
+            </a>
           </div>
-          <div className='border border-gray-500 rounded-md p-5 flex-1'>
-            <h3 className='text-2xl font-semibold mt-8'>Em construção...</h3>
-            <p className='text-gray-400 text-sm mt-4'>Ainda estou desenvolvendo, aguarde um pouco.</p>
-            <button className='mt-5 underline hover:scale-105 transition-transform'>Visualizar o código</button>
+          <div className="border border-gray-500 rounded-md p-5 flex-1">
+            <h3 className="text-2xl font-semibold mt-8">
+              Pedra, papel e tesoura
+            </h3>
+            <p className="text-gray-400 text-sm mt-4">
+              Este projeto é uma solução para um exercício de Machine Learning
+              da freeCodeCamp. O objetivo é criar um jogador inteligente que
+              consiga vencer adversários baseados em padrões com pelo menos 60%
+              de aproveitamento no jogo de pedra, papel e tesoura.
+            </p>
+            <a
+              href="https://github.com/pedrogattosch/rock-paper-scissors"
+              target="_blank"
+              className="mt-5 underline hover:scale-105 transition-transform"
+            >
+              Visualizar o código
+            </a>
           </div>
-        </div>      
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Projects;
