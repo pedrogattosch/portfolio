e97a4280323e22b44874696f2bf93f8e9896fa43
Pedro Miguel Gatto Schardong <pedrogattosch@gmail.com>
2025-12-14 00:14:56 -0300

    feat: update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,17 +14,13 @@ function Skills() {
       <div className="container m-auto px-5 py-10">
         <h2 className="text-xl font-semibold">{content[lang].titulo}</h2>
         <div className="mt-12">
-          <ul className="list-disc flex flex-col gap-5 ml-5 sm:flex-row sm:gap-20">
+          <ul className="list-disc flex flex-col gap-5 ml-5 sm:flex-row sm:flex-wrap sm:gap-20">
             <li>C</li>
             <li>C++</li>
             <li>Python</li>
             <li>JavaScript</li>
             <li>HTML</li>
             <li>CSS</li>
-          </ul>
-        </div>
-        <div className="mt-5">
-          <ul className="list-disc flex flex-col gap-5 ml-5 sm:flex-row sm:gap-20">
             <li>Sass</li>
             <li>Tailwind CSS</li>
             <li>React</li>
